896b1fc97720d0c0c338de9daac33364344dad9f
ErikaStott <erika.r.stott@gmail.com>
2019-07-16 08:40:28 -0700

    added some margins to dashboard

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -43,13 +43,13 @@ export default class Dashboard extends Component{
             <h2 style={{textAlign:'center', marginTop:'50px', fontStyle:"italic"}}>Gift Guide</h2>
             <div id="gift-ideas"></div>
 
-            <h2 style={{textAlign:'center'}}>Featured In</h2>
+            <h2 style={{textAlign:'center', marginTop:"30px"}}>Featured In</h2>
             <div id="articles" style={{display:'flex', flexDirection:'row', justifyContent:'center'}}>
             <a href="https://www.wral.com/-that-s-good-chocolate-is-what-zebulon-mom-grandma-creates/15324117/" target="_"><div id="article-link1"></div></a>
             <a href="https://wakemedvoices.org/2016/05/thats-good-chocolate/" target="_"><div id="article-link2"></div></a>
             </div>
             
-            <h2 style={{textAlign:'center'}}>Follow Us</h2>
+            <h2 style={{textAlign:'center', marginTop:"30px"}}>Follow Us</h2>
             <div id="social-media" style={{display:'flex', flexDirection:'row', justifyContent:'center'}}>
                 <a id="fb-logo" href="https://www.facebook.com/ThatsGoodChocolate" target="_"><div></div></a>
                 <a id="insta-logo" href="https://www.instagram.com/thatsgoodchocolate" target="_"><div></div></a>

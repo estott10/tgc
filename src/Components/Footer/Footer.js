1bc53f8130ba3f7e4c7a936b0b6a4e81ab86234d
ErikaStott <erika.r.stott@gmail.com>
2019-11-19 16:05:59 -0800

    updated store links

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -6,7 +6,7 @@ export default function Footer(){
     return(
         <div className="logo-menu" id="footer" style={{display: "flex", flexDirection:"row", justifyContent:"space-around", backgroundColor: "lightgrey", marginTop:"20px"}}>
             <div className="menu" id="footer-menu" style={{ display: 'flex', color: 'black', flexDirection: 'row', justifyContent: 'space-between'}}>
-                <p><a href="https://thatsgoodchocolate-107815.square.site" style={{ textDecoration: 'none', color: 'black' }}>Order Here</a></p>
+                <p><a href="https://thatsgoodchocolate-107815.square.site/s/shop" style={{ textDecoration: 'none', color: 'black' }}>Order Here</a></p>
                 <p><Link to="/locations" style={{ textDecoration: 'none', color: 'black', padding: "0px 20px", margin: '0px' }}>Locations</Link></p>
                 <p><Link to="/contact" style={{ textDecoration: 'none', color: 'black', padding: "0px 20px", margin: '0px' }}>Contact</Link></p>
                 <p><Link to="/about" style={{ textDecoration: 'none', color: 'black', padding: "0px 20px", margin: '0px' }}>About Us</Link></p>
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -12,7 +12,7 @@ export default function Header(){
             <div className="logo-menu" id="header-menu" style={{display: "flex", flexDirection:"row", justifyContent:"space-between"}}>
                 <div className="logo" style={{width: "100px", height: "100px"}}><a href="https://squareup.com/store/thatsgoodchocolate" style={{width: "200px", height: "200px"}}><div></div></a></div>
                 <div className="menu" style={{ display: 'flex', color: 'black', flexDirection: 'row', justifyContent: 'space-between'}}>
-                    <p><a href="https://thatsgoodchocolate-107815.square.site" style={{ textDecoration: 'none', color: 'black' }}>Order Here</a></p>
+                    <p><a href="https://thatsgoodchocolate-107815.square.site/s/shop" style={{ textDecoration: 'none', color: 'black' }}>Order Here</a></p>
                     <p><Link to="/locations" style={{ textDecoration: 'none', color: 'black', padding: "0px 20px", margin: '0px' }}>Locations</Link></p>
                     <p><Link to="/contact" style={{ textDecoration: 'none', color: 'black', padding: "0px 20px", margin: '0px' }}>Contact</Link></p>
                     <p><Link to="/about" style={{ textDecoration: 'none', color: 'black', padding: "0px 20px", margin: '0px' }}>About Us</Link></p>
